2e6206a575f91610443bd9869fd844864bd2b465
Habunin :D <franklinjoel1821@gmail.com>
2025-05-01 15:10:27 -0400

    refactor(utils): Export Nodo class from new module
    - Adds 'from .nodo import Nodo' to import the extracted Nodo class.
    - Adds 'Nodo' to the '__all__' list to make it available for import via 'from src.utils import Nodo'.

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,7 +2,13 @@
 Utilidades para el Convertidor de Estructuras
 """
 
-from .file_handler import FileHandler
+from .file_handler import FileHandler, DEFAULT_EXCLUDE_PATTERNS
 from .logger import setup_logger
+from .nodo import Nodo
 
-__all__ = ['FileHandler', 'setup_logger']
+__all__ = [
+    'FileHandler',
+    'DEFAULT_EXCLUDE_PATTERNS',
+    'setup_logger',
+    'Nodo'
+    ]
