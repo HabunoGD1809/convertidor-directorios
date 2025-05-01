32664f4406bad6b26318f66b57685219b81a81e6
Habunin :D <franklinjoel1821@gmail.com>
2025-05-01 15:11:13 -0400

    chore: Update application version to 2.0.0
    - Bumps the '__version__' variable to '2.0.0' to reflect significant feature additions and changes.

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ Convertidor de Estructuras de Directorios
 Una aplicación para convertir y visualizar estructuras de directorios.
 """
 
-__version__ = '1.0.0'
+__version__ = '2.0.0'
 __author__ = 'HabunoGD1809'
 
 from .app import ConvertidorDirectorios
